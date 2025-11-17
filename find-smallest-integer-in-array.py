2c6f18210d4194ff97730746695d34fd45f902b5
Katie Weintraub <katie.weint@gmail.com>
2025-11-17 18:38:57 -0500

    Implement find_smallest_int function
    Added function to find the smallest integer in an array.

---
+++ find-smallest-integer-in-array.py
@@ -0,0 +1,15 @@
+'''
+Description:
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+'''
+
+def find_smallest_int(arr):
+  '''
+  Accepts array and returns the smallest integer. 
+  '''
+  return min(arr)
